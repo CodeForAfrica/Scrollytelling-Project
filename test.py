a6c86cc3b97a76a3b48865618a523759a90bfc0b
Arame Thiam <60295234+Arame93@users.noreply.github.com>
2025-08-07 16:48:23 +0000

    Update test.py

--- test.py
+++ test.py
@@ -134,6 +134,16 @@ html_code = """
         .attr("src", `https://flo.uri.sh/story/872914/embed#slide-${slide}`);
     }
 
+    function updateProgress(progress) {
+      // progress en 0..100
+      window.parent.postMessage({streamlitScrollProgress: progress}, '*');
+    }
+    
+    // Par exemple dans handleStepEnter
+    const totalSteps = step.size();
+    const progressPercent = ((response.index + 1) / totalSteps) * 100;
+    updateProgress(progressPercent);
+
     function handleResize() {
       var stepH = Math.floor(window.innerHeight * 0.8);
       stepSel.style("height", stepH + "px");
@@ -156,4 +166,4 @@ html_code = """
 </html>
 """
 
-st.components.v1.html(html_code, height=3000, width= 3000, scrolling=True)
+st.components.v1.html(html_code, height=3000, scrolling=True)
